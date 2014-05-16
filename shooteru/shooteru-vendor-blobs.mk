3f3aa42d278f5add73a50a0fe8aed733bd2e28a0
Darescu Ionut <ionut.darescu@gmail.com>
2014-05-16 07:55:39 -0400

    shooteru: Don't use prebuilt firmwar
    Change-Id: Ic3072c303dea4f7035f0e31e58a629238c48f727

--- shooteru/shooteru-vendor-blobs.mk
+++ shooteru/shooteru-vendor-blobs.mk
@@ -87,26 +87,3 @@ PRODUCT_COPY_FILES += \
     vendor/htc/shooteru/proprietary/lib/libmllite.so:system/lib/libmllite.so \
     vendor/htc/shooteru/proprietary/lib/hw/sensors.shooteru.so:/system/lib/hw/sensors.shooteru.so \
     vendor/htc/shooteru/proprietary/lib/libmlplatform.so:system/lib/libmlplatform.so
- 
-# Firmwares
-PRODUCT_COPY_FILES += \
-    vendor/htc/shooteru/proprietary/firmware/modem.b00:/system/vendor/firmware/modem.b00 \
-    vendor/htc/shooteru/proprietary/firmware/modem.b01:/system/vendor/firmware/modem.b01 \
-    vendor/htc/shooteru/proprietary/firmware/modem.b02:/system/vendor/firmware/modem.b02 \
-    vendor/htc/shooteru/proprietary/firmware/modem.b03:/system/vendor/firmware/modem.b03 \
-    vendor/htc/shooteru/proprietary/firmware/modem.b04:/system/vendor/firmware/modem.b04 \
-    vendor/htc/shooteru/proprietary/firmware/modem.b05:/system/vendor/firmware/modem.b05 \
-    vendor/htc/shooteru/proprietary/firmware/modem.b06:/system/vendor/firmware/modem.b06 \
-    vendor/htc/shooteru/proprietary/firmware/modem.b07:/system/vendor/firmware/modem.b07 \
-    vendor/htc/shooteru/proprietary/firmware/modem.b08:/system/vendor/firmware/modem.b08 \
-    vendor/htc/shooteru/proprietary/firmware/modem.b09:/system/vendor/firmware/modem.b09 \
-    vendor/htc/shooteru/proprietary/firmware/modem.mdt:/system/vendor/firmware/modem.mdt \
-    vendor/htc/shooteru/proprietary/firmware/q6.b00:/system/vendor/firmware/q6.b00 \
-    vendor/htc/shooteru/proprietary/firmware/q6.b01:/system/vendor/firmware/q6.b01 \
-    vendor/htc/shooteru/proprietary/firmware/q6.b02:/system/vendor/firmware/q6.b02 \
-    vendor/htc/shooteru/proprietary/firmware/q6.b03:/system/vendor/firmware/q6.b03 \
-    vendor/htc/shooteru/proprietary/firmware/q6.b04:/system/vendor/firmware/q6.b04 \
-    vendor/htc/shooteru/proprietary/firmware/q6.b05:/system/vendor/firmware/q6.b05 \
-    vendor/htc/shooteru/proprietary/firmware/q6.b06:/system/vendor/firmware/q6.b06 \
-    vendor/htc/shooteru/proprietary/firmware/q6.b07:/system/vendor/firmware/q6.b07 \
-    vendor/htc/shooteru/proprietary/firmware/q6.mdt:/system/vendor/firmware/q6.mdt
